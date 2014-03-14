7a4bae43e1e4b9b8e5d273094badd82b619fd165
Nick Mathews <nickinnik@gmail.com>
2014-03-14 21:35:55 +1100

    MyFirstRealCommit

---
+++ index.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<link href="Main.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>News Feed</title>
+
+</head>
+
+<body>
+
+<?php include( "includes/include_PeericleNav.html" ); ?>
+
+<div id="wrapper">
+
+  <!--Favorites SideBar -->        
+  <div id="favswrapper">
+      	
+        <div class="favs">
+        	<h2>Favorites</h2>
+            	<h3>People</h3>
+                    <a href="index.php">Dr. Michael Davis</a>
+                    <a href="index.php">Dr. Adrienne Oakley</a>
+                    <a href="index.php">Mr. Thomas Bond</a>
+                    <a href="index.php">Mr. Eric Wink</a>
+                    <a href="index.php">Dr. Wendy Ryan</a>
+                    <a href="index.php">Mr. Ryan Flannery</a>
+       	  <h3>Places</h3>
+                    <a href="index.php">Kutztown University</a>
+                    <a href="index.php">Massachussetts Institute of Technology</a>
+                    <a href="index.php">Woods Hole Oceanography Institute</a>
+                    <a href="index.php">Purdue University </a>
+                    <a href="index.php">University of California at Santa Cruz</a>
+                    <a href="index.php">Johns Hopkins</a>
+                    <a href="index.php">Moravian College</a>
+                    <a href="index.php">Loyola University</a>
+                    <br />
+                    <br />
+		</div>
+        <div class="recent">
+            <h2>Recent</h2>
+            	<h3>People</h3>
+                    <a href="index.php">Dr. Loyk Vanderklusyn</a>
+                    <a href="index.php">Dr. Frederic Zolin</a>
+                    <a href="index.php">Dr. Kurt Friehauf</a>
+                    <a href="index.php">Mr. Nick Mathews</a>
+                    <a href="index.php">Dr. Matthew Alford</a>
+                    <a href="index.php">Mr. Flannery</a>
+                <h3>Places</h3>
+                    <a href="index.php">Washington University</a>
+                    <a href="index.php">Clarion University</a>
+                    <a href="index.php">Penn State University</a>
+                    <a href="index.php">Ohio State University</a>
+                    <a href="index.php">University of Victoria</a>
+                    <a href="index.php">Princeton University</a>
+                    <a href="index.php">Swarthmore College</a>
+                    <a href="index.php">Arizona State University</a>
+                    <br />
+                    <br />
+        </div>
+</div>
+        
+        
+        
+        
+        
+        
+<!--Content Area -->        
+      <div id="contentWrapper">
+        <div id="MainColumnWrapper">
+            	<div class="MediaContent">
+                	<div class="ContentProfile">
+                        <a href="index.php"><img class="FloatLeft" src="images/Matthew.JPG" width="124" height="83" alt="Matthew"></a>
+                      <a href="index.php"><h3>Matthew Alford</h3></a>
+                        <a href="index.php"><h4>University of Washington: Applied Physics Lab</h4></a>
+                      <a href="index.php"><h4>Research Project: Samoan Passage Abyssal Mixing Expedition (SPAMEX)</h4></a>
+                        <a href="index.php"><h5>February 15, 2014 7:43 PM</h5></a>
+                    </div>
+                    <br class="ClearFloat">
+                  <a href="index.php">
+                  <h1>We're Underway!</h1></a>
+                  <a href="index.php"><p>We are measuring the turbulence in several of the channels of the Samoan Passage which is arguably the most important place for thermohaline mixing of North Atlantic Deep Water and Antarctic Bottom Water. This data will help climate forcasters in creating a more refined model of ocean circulation and dissolved carbon dioxide in the ocean.  We hope to be completed with the third mooring cruise by the end of February 2014!</p></a>
+					 <div class="media">
+                         <div align="center"><a href="index.php"><img src="images/NickVideo.JPG" width="415" height="276" alt="Mooring Expedition"></a></div>
+					 </div>
+                </div>
+
+
+
+            	<div class="TextContent">
+                	<div class="ContentProfile">
+                        <a href="index.php"><img class="FloatLeft" src="images/James.JPG" width="124" height="83" alt="James"></a>
+                      <a href="index.php"><h3>James Girton</h3></a>
+                        <a href="index.php"><h4>University of Washington: Applied Physics Lab</h4></a>
+                      <a href="index.php"><h4>Research Project: Samoan Passage Abyssal Mixing Expedition (SPAMEX)</h4></a>
+                        <a href="index.php"><h5>February 15, 2014 7:43 PM</h5></a>
+                  </div>
+                  <a href="index.php"><h1>Samoan Passage Mixing</h1></a>
+                  <a href="index.php"><p>I have finished all of the dissolved oxygen, salinity and temperature profiles and the data looks promising!  The next thing we have to do is calculate what the shear is and then apply that to our turbulence data.  Looking forward to getting results!</p></a>
+                    <br class="ClearFloat">
+            	</div>
+
+        
+
+            	<div class="TextContent">
+                	<div class="ContentProfile">
+                        <a href="index.php"><img class="FloatLeft" src="images/Nick.png" width="124" height="124" alt="Nick"></a>
+                      <a href="index.php"><h3>Nick Mathews</h3></a>
+                        <a href="index.php"><h4>Kutztown University: Physical Science Dept.</h4></a>
+                      <a href="index.php"><h4>Research Project: Peericle.org </h4></a>
+                        <a href="index.php"><h5>February 15, 2014 7:43 PM</h5></a>
+                  </div>
+                  <a href="index.php"><h1>Peericle has a Homepage!</h1></a>
+                  <a href="index.php"><p>I've successfully made a website for people to post their research blogs on!  I've named it Peericle (A play on words between Empirical and Peer) because it is a fun word to say and it sort of sounds like the word Empirical and has the social buzzword "peer" in it.  Scientists are often thought of as academics in an ivory tower.  On Peericle you will be able to discuss real science with anyone!  Scientists are your peers on Peericle.org! (I need to come up with a catchier sloagan).</p></a>
+                    <br class="ClearFloat">
+                </div>
+        
+        
+
+
+       	<div class="MediaContent">
+                	<div class="ContentProfile">
+                        <a href="index.php"><img class="FloatLeft" src="images/Chelsea.png" width="124" height="125" alt="Chelsea"></a>
+                      <a href="index.php"><h3>Chelsea Trout</h3></a>
+                        <a href="index.php"><h4>Kutztown University: Physical Science Dept.</h4></a>
+                      <a href="index.php"><h4>Research Project: Wallops Island Beach Migration</h4></a>
+                        <a href="index.php"><h5>February 15, 2014 7:43 PM</h5></a>
+          </div>
+                  <a href="index.php"><h1>Wallops Island is Eroding Fast!</h1></a>
+                  <a href="index.php"><p>I'm running out of things to talk about, so why don't I just say that I'm having a great time typing this paragr.. Wait!  Did you know that the color of blood is actually darker red when it's inside you than when it's outside.  The myth about blue blood is a total racist myth! (Racist because it only looks blue from the outside for white people.  It's actually brown for African Americans and green for Indians.) </p></a>
+					 <div class="media">
+                         <div align="center"><a href="index.php"><img src="images/TomVideo.JPG" width="415" height="276" alt="Wallops Island"></a></div>
+					 </div>
+          </div>
+
+
+
+
+
+            	<div class="MediaContent">
+                	<div class="ContentProfile">
+                        <a href="index.php"><img class="FloatLeft" src="images/Matthew.JPG" width="124" height="83" alt="Matthew"></a>
+                      <a href="index.php"><h3>Matthew Alford</h3></a>
+                        <a href="index.php"><h4>University of Washington: Applied Physics Lab</h4></a>
+                      <a href="index.php"><h4>Research Project: Samoan Passage Abyssal Mixing Expedition (SPAMEX)</h4></a>
+                        <a href="index.php"><h5>February 15, 2014 7:43 PM</h5></a>
+                    </div>
+                    <br class="ClearFloat">
+                  <a href="index.php">
+                  <h1>We're Underway!</h1></a>
+                  <a href="index.php"><p>We are measuring the turbulence in several of the channels of the Samoan Passage which is arguably the most important place for thermohaline mixing of North Atlantic Deep Water and Antarctic Bottom Water. This data will help climate forcasters in creating a more refined model of ocean circulation and dissolved carbon dioxide in the ocean.  We hope to be completed with the third mooring cruise by the end of February 2014!</p></a>
+					 <div class="media">
+                         <div align="center"><a href="index.php"><img src="images/NickVideo.JPG" width="415" height="276" alt="Mooring Expedition"></a></div>
+					 </div>
+                </div>
+
+
+
+            	<div class="TextContent">
+                	<div class="ContentProfile">
+                        <a href="index.php"><img class="FloatLeft" src="images/James.JPG" width="124" height="83" alt="James"></a>
+                      <a href="index.php"><h3>James Girton</h3></a>
+                        <a href="index.php"><h4>University of Washington: Applied Physics Lab</h4></a>
+                      <a href="index.php"><h4>Research Project: Samoan Passage Abyssal Mixing Expedition (SPAMEX)</h4></a>
+                        <a href="index.php"><h5>February 15, 2014 7:43 PM</h5></a>
+                  </div>
+                  <br class="ClearFloat">
+                  <a href="index.php"><h1>Samoan Passage Mixing</h1></a>
+                  <a href="index.php"><p>I have finished all of the dissolved oxygen, salinity and temperature profiles and the data looks promising!  The next thing we have to do is calculate what the shear is and then apply that to our turbulence data.  Looking forward to getting results!</p></a>
+                    <br class="ClearFloat">
+            	</div>
+
+        
+
+            	<div class="TextContent">
+                	<div class="ContentProfile">
+                        <a href="index.php"><img class="FloatLeft" src="images/Nick.png" width="124" height="124" alt="Nick"></a>
+                      <a href="index.php"><h3>Nick Mathews</h3></a>
+                        <a href="index.php"><h4>Kutztown University: Physical Science Dept.</h4></a>
+                      <a href="index.php"><h4>Research Project: Peericle.org </h4></a>
+                        <a href="index.php"><h5>February 15, 2014 7:43 PM</h5></a>
+                  </div>
+                  <a href="index.php"><h1>Peericle has a Homepage!</h1></a>
+                  <a href="index.php"><p>I've successfully made a website for people to post their research blogs on!  I've named it Peericle (A play on words between Empirical and Peer) because it is a fun word to say and it sort of sounds like the word Empirical and has the social buzzword "peer" in it.  Scientists are often thought of as academics in an ivory tower.  On Peericle you will be able to discuss real science with anyone!  Scientists are your peers on Peericle.org! (I need to come up with a catchier sloagan).</p></a>
+                    <br class="ClearFloat">
+                </div>
+        
+        
+
+
+       	<div class="MediaContent">
+                	<div class="ContentProfile">
+                        <a href="index.php"><img class="FloatLeft" src="images/Chelsea.png" width="124" height="125" alt="Chelsea"></a>
+                      <a href="index.php"><h3>Chelsea Trout</h3></a>
+                        <a href="index.php"><h4>Kutztown University: Physical Science Dept.</h4></a>
+                      <a href="index.php"><h4>Research Project: Wallops Island Beach Migration</h4></a>
+                        <a href="index.php"><h5>February 15, 2014 7:43 PM</h5></a>
+          </div>
+                  <a href="index.php"><h1>Wallops Island is Eroding Fast!</h1></a>
+                  <a href="index.php"><p>I'm running out of things to talk about, so why don't I just say that I'm having a great time typing this paragr.. Wait!  Did you know that the color of blood is actually darker red when it's inside you than when it's outside.  The myth about blue blood is a total racist myth! (Racist because it only looks blue from the outside for white people.  It's actually brown for African Americans and green for Indians.) </p></a>
+					 <div class="media">
+                         <div align="center"><a href="index.php"><img src="images/TomVideo.JPG" width="415" height="276" alt="Wallops Island"></a></div>
+					 </div>
+          </div>
+
+		</div>
+	</div>
+
+
+
+
+<!-- Advertisements -->
+<aside>
+
+<!--Marine Tech Ad-->                   
+   		<div class="ads">
+       	    <a href="index.php"><img src="images/BecomeAMarineTech.jpg" alt="Click here if you're interested in becoming a marine technician on a UNOLS ship!" width="160" height="120" /></a>
+          <a href="index.php"><h2>Work on Computers While Sailing the Ocean!</h2></a>
+            <a href="index.php"><p>Become a Marine Tech Intern and get paid to cruise the Pacific Ocean for six months and learn lots of technical skills from experts!</p></a> 
+    </div>
+
+      
+<!--Work at Home Ad -->       
+   		<div class="ads">
+       	    <a href="index.php"><img src="images/Makes50PerDay.jpg" alt="Click here if you're interested in Making $50 per Day!" width="154" height="86" /></a>  
+          <a href="index.php"><h2>Learn How to Make $50/day while Working at Home!</h2></a>
+            <a href="index.php"><p>Olympia Snowe's Book will teach you everything you need to know in order to achieve your dream of not ever leaving your house!!</p></a> 
+       </div>
+       
+       
+<!--Christians Mingle AD-->       
+      		<div class="ads">
+       	    <a href="index.php"><img src="images/ChristiansMingle.jpg" alt="Christians Mingle Pictures Kissing" width="150" height="108" /></a>  
+            <a href="index.php"><h2>Athiests are such a Turn Off!!</h2></a>
+            <a href="index.php"><p>Remember, humans are all sinners condemned to an eternity in Hell unless they are born into a White Christian Family.  Find someone else who is as ignorant and gullible as you on ChristiansMingle.com </p></a> 
+       </div>
+
+       
+<!--Iron Man 3 Ad-->       
+      		<div class="ads">
+       	    <a href="index.php"><img src="images/ironman3.jpg" alt="Iron Man Suit" width="150" height="200" /></a>  
+            <a href="index.php"><h2>Iron Man 4 Coming Soon!!!</h2></a>
+            <a href="index.php"><p>There can never be enough iron man movies!  In this new adventure, Iron Man upgrades his suit to windows 8 and searches for the start button!  Thrilling Action and Authentic Frustration!</p></a> 
+       </div>
+
+       
+<!--Come To KU Ad-->       
+      		<div class="ads">
+       	    <a href="index.php"><img src="images/ComeToKU.jpg" alt="Come to Kutztown University!" width="150" height="77" /></a>  
+            <a href="index.php"><h2>Come to Kutztown!</h2></a>
+            <a href="index.php"><p>A beautiful sprawling campus that hosts almost 11,000 students in a friendly environment that has an inviting scent of manure, the same way that Hershey Park smells like Chocolate.</p></a> 
+       </div>
+
+
+<!--AGU 2014 Ad-->       
+      		<div class="ads">
+       	    <a href="index.php"><img src="images/ConferenceMan.jpg" alt="Man at a Conference" width="150" height="104" /></a>  
+            <a href="index.php"><h2>AGU 2014</h2></a>
+            <a href="index.php"><p>Are you ready for AGU 2014 Fall Meeting?  Register now and Save 15% off of your registration fee!</p></a> 
+       </div>
+
+
+</aside>
+      <p>HelloWorld</p>
+</div> <!-- Closing Wrapper-->                   
+
+
+<?php include("includes/include_PeericleFooter.html"); ?>      
+
+</body>
+
+
+
+
+
+
+
+
+
+
+</html>
